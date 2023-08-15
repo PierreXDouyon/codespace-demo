05bf3ee822cfa94a2407ae356586ee583f672d84
PierreXDouyon <118191281+PierreXDouyon@users.noreply.github.com>
2023-08-15 20:32:54 +0000

    edited index.js, awknowledge a  post w/ thnderclie

--- node_quickstart/index.js
+++ node_quickstart/index.js
@@ -2,29 +2,40 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var bodyParser = require('body-parser')
+
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
-const uri = "<connection string uri>";
-const client = new MongoClient("mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true");
+const uri = "mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true";
+
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
-  res.send('Hello World!')
 
+  console.log(req.body);
 
+  const client = new MongoClient(uri);
 
-//  async function run() {
-//    try {
-//      const database = client.db('codespacetable');
-//      const student = database.collection('student');
-//      // Query for a movie that has the title 'Back to the Future'
-//      const query = { name: 'Peter2', age: 46 };
-//      const result = await student.insertOne(query);
-//      console.log(result);
-//    } finally {
-//      // Ensures that the client will close when you finish/error
-//      await client.close();
-//    }
-//  }
+ async function run() {
+   try {
+     const database = client.db('codespacetable');
+     const student = database.collection('student');
+     // Query for a movie that has the title 'Back to the Future'
+    //  const query = { name: 'Peter2', age: 46 };
+     const result = await student.insertOne(req.body);
+     console.log(result);
+   } finally {
+     // Ensures that the client will close when you finish/error
+     await client.close();
+   }
+ }
+  
+  run().catch(console.dir);
+  
+    res.send('Hello World!')
+  
 })
 
   
