d6b9aeac1cf4777feada4c4b03a023408a640b4d
PierreXDouyon <118191281+PierreXDouyon@users.noreply.github.com>
2023-08-15 20:03:22 +0000

    updated to use post JSON w/ thunderclient

--- node_quickstart/index.js
+++ node_quickstart/index.js
@@ -1,18 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
 const uri = "<connection string uri>";
 const client = new MongoClient("mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true");
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+app.post('/', (req, res) => {
+  res.send('Hello World!')
+
+
+
+//  async function run() {
+//    try {
+//      const database = client.db('codespacetable');
+//      const student = database.collection('student');
+//      // Query for a movie that has the title 'Back to the Future'
+//      const query = { name: 'Peter2', age: 46 };
+//      const result = await student.insertOne(query);
+//      console.log(result);
+//    } finally {
+//      // Ensures that the client will close when you finish/error
+//      await client.close();
+//    }
+//  }
+})
+
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// run().catch(console.dir);
